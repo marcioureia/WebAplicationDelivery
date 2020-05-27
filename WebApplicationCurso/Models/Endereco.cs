ecd5b3dac7813d8d7890afbe9f62499547d36d67
Marcio Andre <marcio_andre@live.com>
2020-05-26 23:48:19 -0300

    creating class user, Endereco, Client

---
+++ WebApplicationCurso/Models/Endereco.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationCurso.Models
+{
+    public class Endereco
+    {
+        [Key]
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Cep { get; set; }
+        public string PontoRef { get; set; }
+
+
+    }
+}
