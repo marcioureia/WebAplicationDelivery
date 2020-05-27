ea6d5f155f0ff302075e6ded45d47fa103bd0da5
Marcio Andre <marcio_andre@live.com>
2020-05-26 23:25:56 -0300

    CRUD Scaffolding

---
+++ WebApplicationCurso/Data/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationCurso.Models
+{
+    public class Context : DbContext
+    {
+        public Context (DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplicationCurso.Models.Department> Department { get; set; }
+    }
+}
--- WebApplicationCurso/Startup.cs
+++ WebApplicationCurso/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationCurso.Models;
 
 namespace WebApplicationCurso
 {
@@ -33,6 +35,9 @@ namespace WebApplicationCurso
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
