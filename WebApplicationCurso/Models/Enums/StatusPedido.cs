80d02c1487cc48b75099bda037831d258e827312
Marcio Andre <marcio_andre@live.com>
2020-05-28 21:57:16 -0300

    Class do porjeto WEBDellivery

---
+++ WebApplicationCurso/Models/Enums/StatusPedido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationCurso.Models.Enums
+{
+    public enum StatusPedido : int
+    {
+        Aberto = 1,
+        Producao = 2,
+        Pronto = 3,
+        Despachado = 4,
+        ACaminho = 5,
+        Entregue = 6
+
+    }
+}
